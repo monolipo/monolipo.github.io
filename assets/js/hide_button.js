e6ac1e5aa9884eb4bb8ffc84cdb207d4812d5a38
Ted Leandro <monolipo.physics@gmail.com>
2026-05-25 22:57:49 -0300

    second update 4734

---
+++ assets/js/hide_button.js
@@ -0,0 +1,88 @@
+function toggleJobDetails() {
+  var elements = document.querySelectorAll('.job-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleManagerDetails() {
+  var elements = document.querySelectorAll('.manager-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function togglePhDMScCommitteesDetails() {
+  var elements = document.querySelectorAll('.phd_msc_comittee-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleTeachDetails() {
+  var elements = document.querySelectorAll('.teach-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleSupervisions() {
+  var elements = document.querySelectorAll('.supervision-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleOutreachDetails() {
+  var elements = document.querySelectorAll('.outreach-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleUpcomingDetails() {
+  var elements = document.querySelectorAll('.upcoming-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleCareerSumamry() {
+  var elements = document.querySelectorAll('.summary-more');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
